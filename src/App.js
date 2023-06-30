53e90418a090ea57010b02aaeb727ccdbe6c2515
Hrvoje Thune <thunehrvoje@gmail.com>
2023-06-30 21:20:52 +0200

    Ispit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [username, setUsername] = useState("");
+  const [userData, setUserData] = useState(null);
+  const [userRepos, setUserRepos] = useState([]);
+
+  const handleChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    fetch(`https://api.github.com/users/${username}`)
+      .then((response) => response.json())
+      .then((data) => setUserData(data));
+
+    fetch(`https://api.github.com/users/${username}/repos`)
+      .then((response) => response.json())
+      .then((data) => setUserRepos(data));
+  };
+
+  const handleReset = () => {
+    setUsername("");
+    setUserData(null);
+    setUserRepos([]);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        <label>
+          GitHub username:
+          <input type="text" value={username} onChange={handleChange} />
+        </label>
+        <button type="submit">Search!</button>
+      </form>
+
+      {userData && (
+        <div>
+          <h2>{userData.name}</h2>
+          <img src={userData.avatar_url} alt={`${userData.name}'s avatar`} />
+          <h3>LOCATION:</h3>
+          <p>{userData.location}</p>
+          <h3>BIO:</h3>
+          <p>{userData.bio}</p>
+
+          <h3>REPOSITORIES:</h3>
+          <ul>
+            {userRepos.map((repo) => (
+              <li key={repo.id}>{repo.name}</li>
+            ))}
+          </ul>
+        </div>
+      )}
+
+      <button onClick={handleReset}>Vrati na početno</button>
     </div>
   );
 }
